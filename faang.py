e4a108786c06405ff78bd1d5ee6557f8e53ed995
Tanya San Juan <tanya.sanjuan@gmail.com>
2025-12-07 23:18:01 +0000

    Create the function in the scrip

--- faang.py
+++ faang.py
@@ -2,6 +2,28 @@
 # for the five FAANG companies: META, AAPL, AMZN, NFLX, and GOOG.
 # And the function called 'plot_data()' loads the most recent CSV file from the 'data/' directory,
 # to create a plot of the closing prices for all 5 stocks, and saves the plot.
-
 # Author: Tanya San Juan
 
+# Dates and times
+import datetime as dt 
+
+# Yahoo Finance data
+import yfinance as yf
+
+# Create a Function 'get_data()'
+def get_data():
+    df = yf.download(
+        ["META", "AAPL", "AMZN", "NFLX", "GOOG"],
+        period="5d",
+        interval="1h")
+
+    # To get the current date and time
+    now = dt.datetime.now()
+    # Format date and time.
+    now.strftime("%Y%m%d-%H%M%S")
+
+    #File name.
+    filename = "../data/" + now.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+# Save CSV
+    df.to_csv("./data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv")
