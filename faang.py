16a0f60f7e68b4c80d9850f9ce681656401da48b
Tanya San Juan <tanya.sanjuan@gmail.com>
2025-12-14 19:32:23 +0000

    minor changes in the comments in the script

--- faang.py
+++ faang.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 '''This program uses the yfinance package to download and visualize hourly stock data
-#for the five FAANG companies: META, AAPL, AMZN, NFLX, and GOOG.
-#And the function called plot_data() loads the most recent CSV file from the data directory, 
-#to create a plot of the closing prices for all 5 stocks, and saves the plot'''
+for the five FAANG companies: META, AAPL, AMZN, NFLX, and GOOG.
+And the function called plot_data() loads the most recent CSV file from the data directory, 
+to create a plot of the closing prices for all 5 stocks, and saves the plot'''
 # Author: Tanya San Juan
 
 # Dates and times
