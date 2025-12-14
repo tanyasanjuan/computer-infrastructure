97e827caf2b1580af9881ae432fd5792d910b591
Tanya San Juan <tanya.sanjuan@gmail.com>
2025-12-14 15:47:19 +0000

    Fixing errors of shebang line. Adding markdowns explaining (shebang and chmd +x)

--- faang.py
+++ faang.py
@@ -1,12 +1,10 @@
-'''This program uses the yfinance package to download and visualize hourly stock data
-for the five FAANG companies: META, AAPL, AMZN, NFLX, and GOOG.
-And the function called 'plot_data()' loads the most recent CSV file from the 'data/' directory,
-to create a plot of the closing prices for all 5 stocks, and saves the plot.
-'''
-# Author: Tanya San Juan
+#!/usr/bin/env python3
 
-# use shebang to run this file directly from the command line
-#!/usr/bin/env python
+'''This program uses the yfinance package to download and visualize hourly stock data
+#for the five FAANG companies: META, AAPL, AMZN, NFLX, and GOOG.
+#And the function called plot_data() loads the most recent CSV file from the data directory, 
+#to create a plot of the closing prices for all 5 stocks, and saves the plot'''
+# Author: Tanya San Juan
 
 # Dates and times
 import datetime as dt 
